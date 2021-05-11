81a083a62df1f46fc1fb0e6bfc91e910caea91f8
Suliman Alkous <sulimanalkous@gmail.com>
2021-05-11 18:17:38 +0300

    TypeScript ready with dependecies

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,20 @@
 // postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    "tailwindcss",
+    "postcss-import",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
 };
