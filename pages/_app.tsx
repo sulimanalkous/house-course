1c8c951e8aea6f16ba28b055369d1d14d5f4172d
Suliman Alkous <sulimanalkous@gmail.com>
2021-05-16 03:42:26 +0300

    fix firebase "NO-APP [DEFAULT]"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { AuthProvider} from "src/auth/useAuth";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AuthProvider>
       <Head>
         <title>Home Sweet Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
