f80eae0c698d6f03bcd7dbda33f71b9a528bd491
Suliman Alkous <sulimanalkous@gmail.com>
2021-05-17 21:09:04 +0300

    implementing Auth page with uid and token

---
+++ src/components/firebaseAuth.tsx
@@ -0,0 +1,29 @@
+import firebase from "firebase/app";
+import { FunctionComponent, useEffect, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import "firebase/auth";
+
+const firebaseAuthConfig = {
+  signInFlow: "popup",
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  signInSuccessUrl: '/',
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+  const [renderAuth, setRenderAuth] = useState(false);
+
+  useEffect(() => {
+    setRenderAuth(true);
+  }, [])
+
+  return <div className="mt-16">
+    {renderAuth ? (<StyledFirebaseAuth uiConfig={firebaseAuthConfig} firebaseAuth={firebase.auth()} />) : null}
+  </div>
+};
+
+export default FirebaseAuth;
