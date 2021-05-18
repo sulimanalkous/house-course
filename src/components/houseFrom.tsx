49315b422e7122fd33c4239b41d0eec42245c9b2
Suliman Alkous <sulimanalkous@gmail.com>
2021-05-18 03:33:22 +0300

    adding Add page with houseFrom

--- src/components/houseFrom.tsx
+++ src/components/houseFrom.tsx
@@ -1,1 +1,49 @@
-export {};
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface IFormData {
+  address: string;
+  latitude: number;
+  longitude: number;
+  bedrooms: string;
+  image: FileList;
+}
+
+interface IProps {}
+
+export default function HouseForm({}: IProps) {
+  const [submiting, setSubmiting] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<IFormData>({ defaultValues: {} });
+
+  useEffect(() => {
+    register("address", { required: "Please enter your address" });
+    register("latitude", { required: true, min: -90, max: 90 });
+    register("longitude", { required: true, min: -180, max: 180 });
+  }, [register]);
+
+  const handleCreate = async (data: IFormData) => {};
+
+  const onSubmit = (data: IFormData) => {
+    setSubmiting(true);
+    handleCreate(data);
+  };
+  return (
+    <form className="mx-auto max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-xl">Add a New House</h1>
+
+      <div className="mt-4">
+        <label htmlFor="search" className="block">
+          Search for your address
+        </label>
+        {/*SEARCH FIELD*/}
+        {errors.address && <p>{errors.address.message}</p>}
+      </div>
+    </form>
+  );
+}
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,1 +1,39 @@
-export {};
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { Image } from "cloudinary-react";
+// @ts-ignore
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
+
+interface IProps {}
+
+interface IViewStates {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export default function Map({}: IProps) {
+  // @ts-ignore
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 29.3759,
+    longitude: 47.9774,
+    zoom: 10,
+  });
+
+  return (
+    <div className="text-black relative">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="calc(100vh - 64px)"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onViewportChange={(nextViewport: IViewStates) => setViewport(nextViewport)}
+        ref={(instance) => (mapRef.current = instance)}
+        minZoom={5}
+        maxZoom={15}
+        mapStyle="mapbox://styles/sulimanalkous/ckot955e6027317ms1x4t67oo"
+      />
+    </div>
+  );
+}
